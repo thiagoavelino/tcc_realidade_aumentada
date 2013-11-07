f04b40cc5e3de3649f8519b23a74a73e4f74247a
ricardo <ricardo@home-PC>
2013-11-06 22:31:46 -0200

    K-Means Funcionando!

---
+++ tcc_realidade_aumentada/KMeansPackage/Ponto.java
@@ -0,0 +1,73 @@
+package KMeansPackage;
+
+
+class Ponto{
+	private float X;
+	private float Y;
+	private int centroid;
+	public Ponto(float x, float y){
+		this.X=x;
+		this.Y=y;
+		this.centroid=0;
+	}
+	public Ponto(float x, float y, int c){
+		this.X = x;
+		this.Y = y;
+		this.centroid = c;
+	}
+	public Ponto(Ponto p){
+		this.X = p.getX();
+		this.Y = p.getY();
+		this.centroid = p.getCentroid();
+	}
+	public float getX(){
+		return this.X;
+	}
+	public float getY(){
+		return this.Y;
+	}
+	public void setX(float a){
+		this.X=a;
+	}
+	public void setY(float b){
+		this.Y=b;
+	}
+	public int getCentroid(){
+		return centroid;
+	}
+	public void setCentroid(int c){
+		centroid = c;
+	}
+	public void add(Ponto p){
+		this.X += p.getX();
+		this.Y += p.getY();
+	}
+	public static Ponto add(Ponto a, Ponto b){
+		return new Ponto(a.getX()+b.getX(),a.getY()+b.getY());
+	}
+	public void addX(float a){
+		this.X+=a;
+	}
+	public void addY(float a){
+		this.Y+=a;
+	}
+	public void divN(int n){
+		this.X/=n;
+		this.Y/=n;
+	}
+	public String toString(){
+		return "("+this.X+","+this.Y+"):"+this.centroid;
+	}
+	public static boolean equalCoordinates(Ponto A, Ponto B){
+		if(A.getX()==B.getX()&&A.getY()==B.getY())
+			return true;
+		else
+			return false;
+	}
+	public static float getDistance(Ponto A, Ponto B){
+		float d = (float)
+			Math.sqrt(Math.pow((A.getX()-B.getX()),2)+Math.pow((A.getY()-B.getY()),2));
+		return d;
+	}
+}
+
---
+++ tcc_realidade_aumentada/KMeansPackage/kMeans.java
@@ -0,0 +1,95 @@
+package KMeansPackage;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+class kMeans{
+	private ArrayList<Ponto> pontos;
+	private ArrayList<Ponto> centroides;
+	public kMeans(ArrayList<Ponto> p){
+		pontos=p;
+		centroides = new ArrayList<Ponto>();
+	}
+	private void setRandomCentroids(int n){
+		Random rnd = new Random(System.nanoTime());
+		ArrayList<Integer> listaItens = new ArrayList<Integer>();
+		int i=0, k;
+		centroides.clear();
+		while(i<n){
+			k=rnd.nextInt(pontos.size());
+			if(!listaItens.contains(k)){
+				listaItens.add(k);
+				centroides.add(new Ponto(pontos.get(k)));
+				//System.out.println(centroides.get(i));
+				i++;
+			}
+		}
+	}
+	private boolean VerificaParada(ArrayList<Ponto> A, ArrayList<Ponto> B){
+		for(int i=0; i<A.size(); i++){
+			if(!Ponto.equalCoordinates(A.get(i),B.get(i)))
+				return false;
+		}
+		return true;
+	}
+	public void ExecutaKMeans(int n){
+		ArrayList<Ponto> V = new ArrayList<Ponto>(n);
+		ArrayList<Integer> nC = new ArrayList<Integer>(n);
+		boolean stop=false;
+		this.setRandomCentroids(n);
+		//System.out.println("\ninicio kmeans");
+		while(!stop){
+			//O proximo for alcula a que centroide o ponto pertence
+			for(int j=0; j<pontos.size(); j++){
+				float d=Integer.MAX_VALUE;
+				for(int i=0; i<n; i++){
+					float dist = Ponto.getDistance(pontos.get(j), centroides.get(i));
+					if(dist<d){
+						d=dist;
+						pontos.get(j).setCentroid(i);
+						//System.out.println(pontos.get(j)+"->"+i);
+					}
+				}
+				//System.out.println(pontos.get(j));
+			}
+			//System.out.println("--\\--");
+			//Os tres proximos for acham o novo centroide
+			V.clear();
+			nC.clear();
+			for(int j=0; j<n; j++){
+				V.add(new Ponto(0,0));
+				nC.add(0);
+			}
+			Ponto pontoTmp;
+			for(int i=0; i<pontos.size(); i++){
+				int index = pontos.get(i).getCentroid();
+				Ponto p = pontos.get(i);
+				Ponto valueV = V.get(index);
+				pontoTmp = Ponto.add(p,valueV);
+				V.set(index,pontoTmp);
+				nC.set(index, nC.get(index)+1);
+			}
+			System.out.println("Soma pontos V");
+			for(int i=0; i<V.size(); i++){
+				//System.out.println(V.get(i));
+				float vx,vy;
+				if(nC.get(i)!=0){
+					vx = V.get(i).getX()/nC.get(i);
+					vy = V.get(i).getY()/nC.get(i);
+				}else{
+					vx = V.get(i).getX();
+					vy = V.get(i).getY();
+				}
+				Ponto pMedia = new Ponto(vx,vy,i);
+				//System.out.println("Media:"+pMedia);
+				V.set(i,pMedia);
+			}
+			//Verifica criério de parada
+			stop=this.VerificaParada(centroides, V);
+			if(!stop){
+				for(int i=0; i<centroides.size(); i++)
+					centroides.set(i,V.get(i));
+			}
+		}
+	}
+}
